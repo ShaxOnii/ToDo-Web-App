e5d71b9017eaa8a1ba74e78cf37c266bce686dff
Karol Śliwka <karlos.kl454@gmail.com>
2025-01-01 12:45:03 +0000

    Testing server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
-  await res.send("Welcome", process.env.TE);
+  await res.send("Welcome", process.env.TEST);
 });
 
 // Static files
