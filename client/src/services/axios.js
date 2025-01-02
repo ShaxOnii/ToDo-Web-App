538e820c49d639370e2624550ffc41bf004cf04d
Karol Śliwka <karlos.kl454@gmail.com>
2025-01-02 11:14:03 +0000

    Fix: Update API URL to use Heroku

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  //baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  //baseURL: "http://localhost:3000",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000",
   withCredentials: true, // Jeśli JWT w ciasteczkach
 });
 
